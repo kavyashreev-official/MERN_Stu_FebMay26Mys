c89f5f7f9b5dce086b997ce8f5ffe696a0314864
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-03-03 12:31:52 +0530

    Arithmetic Operators

---
+++ W3/D2/P1.js
@@ -0,0 +1,13 @@
+//Arithmetic Operators
+console.log("Arithmetic Operators");
+const a=2, b=3;
+console.log("a+b",a+b);
+console.log("a-b",a-b);
+console.log("a*b",a*b);
+console.log("a/b",a/b); 
+console.log("a%b",a%b);  //Modulus
+console.log("a**b",a**b);  //Exponential
+console.log("a++",a++); //post-increment
+console.log("++a",++a); //pre-increment
+console.log("a--",a--); //post-decrement
+console.log("--a",--a); //pre-decrement
