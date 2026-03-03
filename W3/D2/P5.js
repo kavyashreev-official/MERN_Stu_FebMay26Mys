e92c347d5b767f39272fdf50a86312f4705b2800
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-03-03 12:33:47 +0530

    Function Declaration

---
+++ W3/D2/P5.js
@@ -0,0 +1,8 @@
+//Function Declaration
+// function greet(name); //it is n
+// Function Declaration/Definition
+function greet(name) {
+    console.log("Hello, "+name+" !");    
+}
+//Function call
+greet("Kavya");
