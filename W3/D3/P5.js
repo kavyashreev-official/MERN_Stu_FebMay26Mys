886ce5a56105228dfe09bebeab0541870c291f2f
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-03-04 12:40:03 +0530

    Basics of Objects

---
+++ W3/D3/P5.js
@@ -0,0 +1,10 @@
+//Basics of Objects
+const person={
+    name:'Rahul',
+    age:30,
+    isStudent:false
+};
+console.log("Person",person);
+console.log("Name:",person['name']);
+console.log("Age:",person['age']);
+console.log("Is student:",person['isStudent']);
