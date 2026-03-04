36d717726b160909bc02381c56c0a38d82aba2c8
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-03-04 12:38:32 +0530

    while &do-while loop

--- W3/D3/P2.js
+++ W3/D3/P2.js
@@ -1,10 +1,10 @@
 //while loop
-// console.log("while loop");
-// let count=0;
-// while(count<5){
-//     console.log("count: ",count);
-//     count++;
-// }
+console.log("while loop");
+let count=0;
+while(count<5){
+    console.log("count: ",count);
+    count++;
+}
 
 //do-while loop
 let value=10;
