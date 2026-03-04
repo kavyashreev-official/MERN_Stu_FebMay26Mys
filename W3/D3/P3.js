6f9db0d4d3105bf11e26abf9baf0ab36c602c75b
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-03-04 12:39:12 +0530

    break & continue statements

---
+++ W3/D3/P3.js
@@ -0,0 +1,16 @@
+// break statement
+for(let i=0;i<5;i++){
+    if(i===2){
+        console.log("i:", i);
+        break;
+    }
+}
+
+//continue
+for(let i=0;i<5;i++){
+    if(i===2){
+        console.log("i=", i);
+        continue;
+    }
+    console.log("i=", i);
+}
