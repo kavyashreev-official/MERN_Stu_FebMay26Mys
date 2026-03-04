ce12643290d3d8fe5b026afa24d597bf3e3aeb48
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-03-04 14:04:37 +0530

    Basics of Objects & Object Constructor

--- W3/D3/P5.js
+++ W3/D3/P5.js
@@ -8,3 +8,23 @@ console.log("Person",person);
 console.log("Name:",person['name']);
 console.log("Age:",person['age']);
 console.log("Is student:",person['isStudent']);
+
+//Add a new property 
+person.city="Mysore";
+console.log("Person",person);
+
+//Modify 
+person.age=31;
+console.log("Person",person);
+
+//Delete
+delete person.isStudent;
+console.log("Person",person); 
+
+
+//Object Constructor 
+const car=new Object();
+car.make="Adui";
+car.model="A4";
+car.year=2026;
+console.log("Car info",car);
