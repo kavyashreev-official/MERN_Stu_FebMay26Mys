2b356672eff33419f35950b59d7d9ec92c279bce
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-03-05 22:48:52 +0530

    Reduce function

---
+++ W3/D4/P5.js
@@ -0,0 +1,15 @@
+//Reduce function
+let nums=[5,10,15];
+let total=nums.reduce((intermediate,current)=>intermediate+current,0);
+console.log(total);
+let avg
+
+
+//reduce to object count by category
+let items=["pen","pencil","pen","eraser"];
+let count=items .reduce((intermediate,item)=>{
+    a[item]=(a[b]||0+1);
+
+    return a;
+},{});
+console.log("Item count: ",count);
