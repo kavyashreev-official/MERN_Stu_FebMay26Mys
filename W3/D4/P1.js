db509fa51de0d27d11a022c836b2f93bbda8fcdc
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-03-05 22:47:16 +0530

    Array Basics

---
+++ W3/D4/P1.js
@@ -0,0 +1,30 @@
+//Array Basics
+console.log("Array Basics");
+//creating arrays
+let emptyArray=[];
+let numArray=[1,2,3,4];
+let mixArray=[42,"hello",true,null,{name:"Gola"},[5,6]];
+console.log(emptyArray);
+console.log(numArray);
+console.log(mixArray);
+
+//using constructor
+let fruits=new Array("Apple","Mango");
+console.log(fruits);
+console.log("Is fruits an array?",Array.isArray(fruits));
+
+//Push- to add a new value to array
+fruits.push("Cherry");
+console.log(fruits);
+
+//pop-delete(remove)
+fruits.pop();
+console.log(fruits);
+
+//unshift- adds elements to beginning
+fruits.unshift("Orange");
+console.log(fruits);
+
+//shift-remove from beginning
+fruits.shift();kl 
+console.log(fruits);
