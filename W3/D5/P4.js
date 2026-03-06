0c9e2718cf8d7878e5e24f0374a8ea2d6bda4a45
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-03-06 13:08:36 +0530

    Keyboard Event: keydown

---
+++ W3/D5/P4.js
@@ -0,0 +1,6 @@
+const input=document.getElementById("nameInput");
+const display=document.getElementById("display");
+input.addEventListener("keydown",function(event){
+    console.log("Key pressed:",event.key);
+    display.textContent="Last key pressed:", +event.key;
+});
