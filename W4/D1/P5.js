456c723836e9f87cd32fa23f286940c37c31a132
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-03-09 15:25:15 +0530

    Breakpoint

---
+++ W4/D1/P5.js
@@ -0,0 +1,23 @@
+//Breakpoint
+// it  is used for following reasons
+//inspect the variable values,
+// view the call stack
+// Step throught code line by line 
+// Evaluate expression in the console
+// watch how variables change during the execution
+//to find logical error
+//browser 
+
+function calculateTotal(prices){
+    let total=0;
+    for(let i=0;i<prices.length;i++){
+        let price= prices[i];
+        debugger;
+        total+=price;
+    }
+    return total;
+}
+
+let cart=[100,250,150,1000,220];
+
+console.log("Total: ",calculateTotal(cart));
