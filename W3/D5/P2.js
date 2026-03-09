d74613dc51d6eaf36386684cf743d92f0a6b80d8
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-03-09 15:16:11 +0530

    Event object in click

--- W3/D5/P2.js
+++ W3/D5/P2.js
@@ -1,11 +1,12 @@
 const box=document.getElementById("box");
 const insideBtn=document.getElementById("insideBtn");
-box.addEventListener("click",function(){
+box.addEventListener("click",function(event){
     console.log("event type",event.type);
     console.log("event target id",event.target.id);
     console.log("event current target id",event.currentTarget.id);
 });
-insideBtn.addEventListener("click",function(){
+insideBtn.addEventListener("click",function(event){
+    event.stopPropagation();
     console.log("event type",event.type);
     console.log("event target id",event.target.id);
     console.log("event current target id",event.currentTarget.id);
