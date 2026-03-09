d01d2ce618efb1bc9b746b27c50f677409c3eb9a
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-03-09 17:34:36 +0530

    finally, return in catch block

--- W4/D1/P6.js
+++ W4/D1/P6.js
@@ -1,32 +1,33 @@
 //finally
 function example(){
     try{
-        console.log("Example in try block");
+        console.log("example in try block");
         return "TRY_RETURN";
     }
-    finally{
-        console.log("This is printed");
-    }
-}
-console.log("Example result: ",example());
 
-//return in catch block 
-function example() {
-    try {
-        try {
-            throw new Error("New error");
-        }
-        catch (e) {
-            console.log("Example 1:  caught error");
-            // return 10;
-            throw (e);
-        }
-        finally {
-            console.log("Example 1: finally still runs");
-        }
+finally{
+    console.log("this is priented:");
+}
+}
+console.log("Example result:",example());
+
+//return in catch block
+function example(){
+    try{
+    try{
+        throw new Error("New error")
     }
     catch(e){
-        console.log("Example 1 outer catch",e.message());
+        console.log("Example 1: caught error");
+       // return 10;
+       throw(e);
+    }
+    finally{
+        console.log("Example 1: caught error");
     }
 }
-console.log("Example 1: ", example());
+catch(e){
+    console.log("Example 1 outer catch",e.message);
+}
+}
+console.log("Example 1:",example());
