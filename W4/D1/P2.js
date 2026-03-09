b0c620490006c902a4ae0459c731bffed5d217e1
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-03-09 15:22:33 +0530

    try..catch..finally

---
+++ W4/D1/P2.js
@@ -0,0 +1,18 @@
+//try..catch..finally
+ 
+function parseConfig(text){
+    try{
+        let config= JSON.parse(text);
+        console.log(config);
+        return config;
+    }
+    catch(err){
+        console.log("Error parsing config: ", err.message);
+        return null;
+    }
+    finally{
+        console.log("Finally block executed.");
+    }
+}
+parseConfig('{"theme":"dark"}'); //valid json becoz it has key-value pair in it
+parseConfig('{json}'); //invalid json
