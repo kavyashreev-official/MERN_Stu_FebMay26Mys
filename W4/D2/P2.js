2f765f8590f49cedb6a23092bca44abda1197a53
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-03-10 10:35:53 +0530

    CreateElement() & appendChild()

---
+++ W4/D2/P2.js
@@ -0,0 +1,22 @@
+// CreateElement() & appendChild()
+
+const list=document.getElementById("list");
+let counter=1;
+
+document.getElementById("addBtn").addEventListener("click",function(){
+    const li=document.createElement("li");    
+    li.textContent="Item " + counter++;
+    list.appendChild(li);
+    errMessage.innerText="";
+    
+});
+document.getElementById("rmBtn").addEventListener("click",
+    function(){
+        if(list.lastElementChild){
+        list.removeChild(list.lastElementChild);
+        }
+        else{
+           errMessage.textContent="No Item to delete"; // should create new id for it not use list.textContent
+        };
+
+    });
