5dcafe6b0d6f7bd80be0a75f47a853bddf0bf568
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-03-11 17:10:42 +0530

    JSON Audit on a Single Repor

---
+++ Assignments/JSON Audit-Assg6.js
@@ -0,0 +1,32 @@
+// JSON Audit on a Single Repor
+function auditReport(reportJSON) {
+
+  const report = JSON.parse(reportJSON);
+
+  let okCount = 0;
+  let failCount = 0;
+
+  for (let module in report.modules) {
+
+    if (report.modules[module] === "OK") {
+      okCount++;
+    }
+
+    if (report.modules[module] === "FAIL") {
+      failCount++;
+      break;
+    }
+  }
+
+  const summary = {
+    okCount: okCount,
+    failCount: failCount
+  };
+
+  const summaryJSON = JSON.stringify(summary);
+
+  return {
+    summary: summary,
+    summaryJSON: summaryJSON
+  };
+}
