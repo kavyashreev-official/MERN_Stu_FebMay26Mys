aef720219a4a5e882120311ec06723d10b5c0d33
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-03-23 12:32:04 +0530

    CommonJS export and import

---
+++ W6/D1/P2.js
@@ -0,0 +1,13 @@
+// CommonJS export and import
+
+function calculateTax(amount){
+    return amount*0.18;
+}
+function applyDiscount(amount,percent){
+    return amount-amount*(percent/100);
+}
+function formatCurrency(amount){
+    return "INR "+amount.toFixed(2);
+}
+//module.exports makes function available to requie()
+module.exports={calculateTax,applyDiscount,formatCurrency};
