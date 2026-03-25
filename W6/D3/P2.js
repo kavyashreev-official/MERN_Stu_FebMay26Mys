de9cf16fe71ee55bc287a306bac57aad98f94270
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-03-25 10:41:55 +0530

    Reading & writing files synchronously

--- W6/D3/P2.js
+++ W6/D3/P2.js
@@ -5,11 +5,22 @@ const path=require("path");
 
 const filePath=path.join(__dirname,"sync-note.txt");
 
-fs.writeFileSync(filePath,"This file was written using writeFileSync().\n Synchronous operation block execution");
+// fs.writeFileSync(filePath,"This file was written using writeFileSync().\n Synchronous operation block execution");
 
+//Append to a file
+fs.appendFile(filePath,"New text added using fs.appendFile.",
+    function(error){
+        if(error){
+            console.log("Append error: ",error.message);
+            return;
+        }
+        console.log("File content appended.");
+    }
+);
 console.log("File written synchronously.");
 
 const content=fs.readFileSync(filePath,"utf-8");
 
 console.log("File read synchronously.");
+console.log("File content:");
 console.log(content);
