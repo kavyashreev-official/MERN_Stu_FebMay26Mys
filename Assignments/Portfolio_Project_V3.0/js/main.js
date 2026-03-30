a98032c4558b06424fb325b52366f48ea3c9e4a8
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-03-30 13:50:42 +0530

    main.js

---
+++ Assignments/Portfolio_Project_V3.0/js/main.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded",
+    function(){
+        // Back to top
+        backToTop();
+        // contact-validation
+        initContactValidation();
+        // Greetings
+        greetings();
+        //modal
+        initModal();
+        //projects
+        renderProjects();
+        // Scrolling
+        scroll();
+        //Render skills
+        renderSkills();       
+        //theme
+        initThemeToggle();
+        
+    }
+)
