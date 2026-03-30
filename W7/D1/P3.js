e591df836bba0ac402f76cc1e94c27b9cdd04e3a
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-03-30 13:04:59 +0530

    Handling different HTTP methods in express

--- W7/D1/P3.js
+++ W7/D1/P3.js
@@ -1,11 +1,17 @@
 // Handling different HTTP methods in express
 const express=require("express");
+const { message } = require("statuses");
 
 const app=express();
 
 //To read
 app.get("/users",function(req,res){
-    res.send("Returning all useers");
+    res.status(200).json([{message:"Success"},
+        {id:1,name:"Kavya"},
+        {id:2,name:"Bipin"},
+        {id:3,name:"Keerthi"},
+    ]);
+    res.send("Returning all users");
 });
 
 //To create
