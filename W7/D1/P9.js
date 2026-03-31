e6cdea3ddc5e5fa857bd454d4bab2d3832df43fa
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-03-31 15:12:22 +0530

    Middleware chaining and error handling middleware

---
+++ W7/D1/P9.js
@@ -0,0 +1,36 @@
+// Middleware chaining and error handling middleware
+
+const express=require("express");
+const { message } = require("statuses");
+const app=express();
+
+// First middleware
+// Global middleware
+app.use(function(req,res,next){
+    console.log("Request:",req.method,req.url);
+    next();
+});
+app.use(function(req,res,next){
+    req.requestSource="middleware-chain-example";
+    next();
+});
+app.get("/ok",function(req,res){
+    res.json({
+        success:true,
+        source:req.requestSource
+    });
+});
+app.get("/fail",function(req,res,next){
+    next(new Error("Route failure"));
+});
+
+// Error handling: global error handler(any error will be handled)
+app.use(function(error,req,res,next){
+    res.status(500).json({
+        success:false,
+        message:error.message
+    });
+});
+app.listen(4000,function(){
+    console.log("Express server running at http://localhost:4000");
+});
