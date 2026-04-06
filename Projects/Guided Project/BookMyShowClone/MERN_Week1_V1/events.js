ddb88a254ccb14fd4ba7d21d11d7300862c6765d
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-04-06 10:29:52 +0530

    This file creates & exports a custom EventEmitter instance

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week1_V1/events.js
@@ -0,0 +1,7 @@
+// This file creates & exports a custom EventEmitter instance
+
+const EventEmitter=require("events");
+// Custom EventEmitter object
+const bookingEmitter=new EventEmitter();
+
+module.exports=bookingEmitter;
