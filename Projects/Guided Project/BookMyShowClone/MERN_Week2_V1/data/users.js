f9354407596a0a348c927efd4f7b7aac329b5673
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-04-08 10:06:59 +0530

    User's details of the application

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/data/users.js
@@ -0,0 +1,20 @@
+// User's details of the application
+
+const user=[
+    {
+        id:1,
+        name:"Kavya",
+        email:"u@e.com",
+        password:"kavya@123",
+        role:"user"
+    },
+    {
+        id:2,
+        name:"Admin",
+        email:"a@e.com",
+        password:"admin@123",
+        role:"admin"
+    }
+];
+
+module.exports=users;
