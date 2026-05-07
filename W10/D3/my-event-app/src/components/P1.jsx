08ca85aa684c284fc758bd6f451032ce920fd663
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-05-07 09:59:59 +0530

    Basic events in React

--- W10/D3/my-event-app/src/components/P1.jsx
+++ W10/D3/my-event-app/src/components/P1.jsx
@@ -8,13 +8,13 @@ export function EventBasics(){
     // Declaring a event handler function
     const handleClick=()=>alert("Clicked");
 
-    // return(
-    //     <section>
-    //         <h2>Basic Events</h2>
-    //         {/* Event binding */}
-    //         <button onClick={handleClick}>
-    //             Click me
-    //         </button>
-    //     </section>
-    // )
+    return(
+        <section>
+            <h2>Basic Events</h2>
+            {/* Event binding */}
+            <button onClick={handleClick}>
+                Click me
+            </button>
+        </section>
+    )
 }
