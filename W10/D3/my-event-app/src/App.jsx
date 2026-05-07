27fd092b20006208b3ace85da0eb8c8ce0ea9694
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-05-07 10:31:40 +0530

    React Event Object

--- W10/D3/my-event-app/src/App.jsx
+++ W10/D3/my-event-app/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import {EventBasics} from "./components/P1"
-
+import {EventObject} from "./components/P2"
+// import {EventBasics} from "./components/P1"
+// import {EventBasics} from "./components/P1"
 
 function App() {
   return (
     <>
-      <EventBasics />
+      {/* <EventBasics /> */}
+      <EventObject />
     </>
   )
 }
