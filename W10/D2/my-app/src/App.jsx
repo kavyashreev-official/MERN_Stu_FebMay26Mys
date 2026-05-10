eb8b787139c296be66b1464159868e77a7c2e838
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-05-10 22:02:21 +0530

    useCallback hook

--- W10/D2/my-app/src/App.jsx
+++ W10/D2/my-app/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 import {UseContextIntro} from "./components/P1";
 import {CustomHooksIntro} from "./components/P2";
+import {UserRefIntro} from "./components/P3";
+import {UseReducerIntro} from "./components/P4"
+import {UseCallbackIntro} from "./components/P5"
 
 function App() {
   return (
     <>
      {/* <UseContextIntro /> */}
-     <CustomHooksIntro />
+     {/* <CustomHooksIntro /> */}
+     {/* <UserRefIntro /> */}
+     {/* <UseReducerIntro /> */}
+     <UseCallbackIntro />
     </>
   )
 }
