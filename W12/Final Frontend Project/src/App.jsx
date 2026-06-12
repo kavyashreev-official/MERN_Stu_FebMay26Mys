265340b07e2c7fa4637225ad617ba329889cdc22
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-06-12 11:42:46 +0530

    Final BMS Frontend App

---
+++ W12/Final Frontend Project/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  
+  return (
+      <div>
+
+            <h1>
+                BookMyShow Clone
+            </h1>
+
+            <p>
+                Welcome to BookMyShow Frontend.
+            </p>
+
+        </div>
+
+  )
+}
+
+export default App
