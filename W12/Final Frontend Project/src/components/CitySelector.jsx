c40ad179dff6af3480b9743f50eef07ee08c851f
kavyashreev-official <kavyashreev.2703@gmail.com>
2026-06-12 12:30:14 +0530

    State concept

---
+++ W12/Final Frontend Project/src/components/CitySelector.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+export default function CitySelector(){
+    const [city,setCity] = useState("Mysuru");
+    return(
+        <section>
+            <h2>Selected City</h2>
+            <p>{city}</p>
+            <button
+              onClick={()=>{
+                setCity("Mandya");
+              }}>Mandya</button>
+            <button
+              onClick={()=>{
+                setCity("Mandya");
+              }}>Bengaluru</button>
+        </section>
+    );
+
+}
